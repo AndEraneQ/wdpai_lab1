c0fa8b1ff3102c0c42944f7dac56f6afffb08d28
Piotr <trojanpiotrek@interia.pl>
2024-11-07 21:43:04 +0100

    updated to lab2

--- python_server/server.py
+++ python_server/server.py
@@ -1,33 +1,46 @@
 import json
-import random
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from typing import Type
+import psycopg2 
+import os
+import time
+
+DB_HOST = os.environ.get('DB_HOST', 'postgres')
+DB_PORT = int(os.environ.get('DB_PORT', 5432))
+DB_NAME = os.environ.get('DB_NAME', 'mydatabase')
+DB_USER = os.environ.get('DB_USER', 'myuser')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'mypassword')
+
+def connect_to_db():
+    while True:
+        try:
+            conn = psycopg2.connect(
+                host=DB_HOST,
+                port=DB_PORT,
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD
+            )
+            print("Połączono z bazą danych")
+            return conn
+        except psycopg2.OperationalError:
+            print("Błąd połączenia z bazą danych, ponawianie za 5 sekund...")
+            time.sleep(5)
+
+conn = connect_to_db()
+cursor = conn.cursor()
+
 
 
 # Define the request handler class by extending BaseHTTPRequestHandler.
 # This class will handle HTTP requests that the server receives.
-class SimpleRequestHandler(BaseHTTPRequestHandler):
 
-    user_list = [
-            {
-                'id': 1,
-                'first_name': 'Michal',
-                'last_name': 'Mucha',
-                'role': 'Instructor'
-            },
-            {
-                'id': 2,
-                'first_name': 'John',
-                'last_name': 'Doe',
-                'role': 'Student'
-            },
-            {
-                'id': 3,
-                'first_name': 'Jane',
-                'last_name': 'Austen',
-                'role': 'Designer'
-            }
-        ]
+def fetch_users():
+    cursor.execute("SELECT id, first_name, last_name, role FROM users")
+    users = cursor.fetchall()
+    return [{"id": user[0], "first_name": user[1], "last_name": user[2], "role": user[3]} for user in users]
+
+class SimpleRequestHandler(BaseHTTPRequestHandler):
 
     def do_OPTIONS(self):
 
@@ -43,41 +56,51 @@ class SimpleRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-        self.wfile.write(json.dumps(self.user_list).encode()) # WARNING: user_list hardcoded
+        user_list = fetch_users()
+
+        self.wfile.write(json.dumps(user_list).encode()) # WARNING: user_list hardcoded
 
     def do_POST(self) -> None:
         content_length: int = int(self.headers['Content-Length'])
         post_data: bytes = self.rfile.read(content_length)
         received_data: dict = json.loads(post_data.decode())
 
-        newUser = {
-            'id': random.randint(1, 1000000),
-            'first_name': received_data['firstName'],
-            'last_name': received_data['lastName'],
-            'role': received_data['role']
-        }
-        self.user_list.append(newUser)
+        first_name = received_data.get('firstName')
+        last_name = received_data.get('lastName')
+        role = received_data.get('role')
+
+        cursor.execute(
+            "INSERT INTO users (first_name, last_name, role) VALUES (%s, %s, %s)",
+            (first_name, last_name, role)
+        )
+        conn.commit()
+
+        user_list = fetch_users()
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(json.dumps(self.user_list).encode())
+        self.wfile.write(json.dumps(user_list).encode())
 
     def do_DELETE(self) -> None:
         content_length: int = int(self.headers['Content-Length'])
         post_data: bytes = self.rfile.read(content_length)
         received_data: dict = json.loads(post_data.decode())
 
-        user_id = received_data['id']
-        SimpleRequestHandler.user_list = [user for user in self.user_list if user['id'] != user_id]
+        user_id = received_data.get('id')
+
+        cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
+        conn.commit()
+
+        user_list = fetch_users()
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
-        self.wfile.write(json.dumps(self.user_list).encode())
+        self.wfile.write(json.dumps(user_list).encode())
 
 def run(
         server_class: Type[HTTPServer] = HTTPServer,
